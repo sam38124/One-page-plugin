87951859801881cbc3a5fce1d93dccaf7f28c267
homee-jianzhi <116696497+HomeeJianzhi@users.noreply.github.com>
2023-04-13 23:52:48 +0800

    fix ui

--- src/glitterBundle/plugins/plugin-creater.js
+++ src/glitterBundle/plugins/plugin-creater.js
@@ -25,7 +25,6 @@ export class Plugin {
                     }, 100);
                 }
                 fal += 1;
-                console.log('error' + url);
             }
         }
         tryLoop();
